e09fd8c6b8bfad516d0219df9e1b2c25c7792d15
yangfang <15868178345@163.com>
2016-07-28 12:39:54 +0800

    second

--- app/src/main/java/com/demo/panguso/demo160714/adapter/ViewPagerAdapter.java
+++ app/src/main/java/com/demo/panguso/demo160714/adapter/ViewPagerAdapter.java
@@ -14,6 +14,7 @@ import java.util.Map;
 public class ViewPagerAdapter extends FragmentPagerAdapter {
     private List<Map<String, Object>> list;
     private List<Fragment> fragments = new ArrayList<>();
+
     private List<String> fragmentTitles = new ArrayList<>();
 
     public ViewPagerAdapter(FragmentManager fm) {
